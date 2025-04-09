4580819f2b4dc5782a142f98c59995a6b85b3821
Azeel Sajjad <aasajjad05@gmail.com>
2025-04-08 22:13:59 -0400

    set up

---
+++ app/(root)/(tabs)/maps.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Maps = () => {
+    return (
+        <SafeAreaView>
+            <Text>Maps</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Maps;
---
+++ app/(root)/(tabs)/ranks.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Ranks = () => {
+    return (
+        <SafeAreaView>
+            <Text>Ranks</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Ranks;
