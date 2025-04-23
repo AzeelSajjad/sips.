39b3886b02e2b527a4a40fec57ed266c5f348346
Azeel Sajjad <aasajjad05@gmail.com>
2025-04-23 13:24:09 -0400

    m

--- backend/models/Friends.ts
+++ backend/models/Friends.ts
@@ -4,3 +4,24 @@ export interface IFRIENDS extends Document {
     user: mongoose.Types.ObjectId;
     created_at: Date;
 }
+
+const FriendsSchema: Schema = new Schema({
+    friend: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    created_at: {type: Date, default: Date.now}
+}, {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+        versionKey: false,
+        transform: (_, ret) => {
+            ret.id = ret._id.toString();
+            delete ret._id;
+        }
+    },
+    toObject: {
+        virtuals: true,
+    },
+});
+
+export default mongoose.model<IFRIENDS>('Friends', FriendsSchema);
