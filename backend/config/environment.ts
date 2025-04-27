4dc9c38347fe67579a095d2aa72d1af4aea6df8e
Azeel Sajjad <aasajjad05@gmail.com>
2025-04-27 15:07:06 -0400

    m

--- backend/config/environment.ts
+++ backend/config/environment.ts
@@ -7,3 +7,13 @@ export interface Enviornment {
         expires_in: string;
     };
 }
+
+export const environment: Enviornment = {
+    nodeEnv: process.env.NODE_ENV || 'development',
+    port: parseInt(process.env.PORT || '3000', 10),
+    dbUri: process.env.MONGO_URI || '',
+    jwt: {
+        secret: process.env.JWT_SECRET || '',
+        expires_in: process.env.EXPIRES_IN || '2d'
+    },
+};
