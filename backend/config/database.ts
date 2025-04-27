7595585fa23b4409a7f9670d7b7d291a2e428ab3
Azeel Sajjad <aasajjad05@gmail.com>
2025-04-27 15:34:00 -0400

    m

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
+import { environment } from "./environment";
 
-const connectToDatabase = () => {
-    
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(environment.dbUri);
+    } catch (error) {
+        process.exit(1);
+    }
 }
