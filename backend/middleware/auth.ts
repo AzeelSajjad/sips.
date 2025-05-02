364f9df864a466bd4d13c49433f35491e33cca49
Azeel Sajjad <aasajjad05@gmail.com>
2025-05-01 20:41:27 -0400

    m

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -1,5 +1,19 @@
 import {Request, Response, NextFunction} from 'express';
+import jwt, {JwtPayload} from 'jsonwebtoken';
+import { environment } from '../config/environment';
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader != null){
+        const token =  authHeader.split(' ')[1];
+        try {
+            const decoded = jwt.verify(token, environment.jwt.secret);
+            
+        } catch (error) {
+            res.status(403).json({ message: 'Invalid or expired token provided.' });
+        }
+    } else {
+        res.status(401).json({ message: 'No token provided.' });
+    }
 
 };
