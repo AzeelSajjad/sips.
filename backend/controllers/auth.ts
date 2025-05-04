96ac8ea2a389c58e32f992aee0e54e64dc946412
Azeel Sajjad <aasajjad05@gmail.com>
2025-05-04 10:00:41 -0400

    m

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -2,8 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import Users from '../models/Users';
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { environment } from '../config/environment';
 
-export const signUp = (req: Request, res: Response, next: NextFunction) => {
+export const signUp = async (req: Request, res: Response) => {
     const {email, password, name} = req.body
-    
+    const salt = await bcrypt.genSalt()
+    const hashed = await bcrypt.hash(password, salt)
+    const newUser = await Users.create({email, name, password: hashed})
+    const newJWT = jwt.sign({ userId: newUser._id }, environment.jwt.secret, { expiresIn: '1h' });
+    return res.status(201).json({ token: newJWT });
 };
