df4278ad2e299418955de35210b9d28d663691e0
Azeel Sajjad <aasajjad05@gmail.com>
2025-05-09 16:41:47 -0400

    created the preference model for the rating algo

--- backend/models/Preference.ts
+++ backend/models/Preference.ts
@@ -1,5 +1,28 @@
 import mongoose, {Schema, Document} from "mongoose"
 
 export interface IPREFERENCE extends Document {
-    
+    user: mongoose.Types.ObjectId;
+    preferred: mongoose.Types.ObjectId;
+    against: mongoose.Types.ObjectId;
+    createdAt: Date;
 }
+
+const PreferenceSchema: Schema = new Schema({
+    user: {type: mongoose.Types.ObjectId, ref: 'User', required: true},
+    preferred: {type: mongoose.Types.ObjectId, ref: 'Drinks', required: true},
+    against: {type: mongoose.Types.ObjectId, ref: 'Drinks', required: true},
+    createdAt: {type: Date, default: Date.now}
+},{
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+        versionKey: false,
+        transform: (_, ret) => {
+            ret.id = ret._id.toString();
+            delete ret._id;
+        }
+    },
+    toObject: {
+        virtuals: true,
+    },
+});
