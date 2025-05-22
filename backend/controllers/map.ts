1b2e495d1017caaff348f272c24b63532fed2ba4
Azeel Sajjad <aasajjad05@gmail.com>
2025-05-22 17:15:29 -0400

    m

--- backend/controllers/map.ts
+++ backend/controllers/map.ts
@@ -7,3 +7,16 @@ import dotenv from 'dotenv'
 dotenv.config()
 import axios from 'axios'
 
+export const searchCafeByName = async (req: Request, res: Response) => {
+    const {name, loc} = req.query
+    if(!name || !loc){
+        res.status(400).json({message: 'Missing Fields'})
+    }
+    const api_key = process.env.GOOGLE_MAP_API_KEY
+    const radius = 3000
+    const type = 'cafe'
+    const encodedName = encodeURIComponent(name as string)
+    const encodedLoc = encodeURIComponent(loc as string)
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedLoc}&key=${api_key}`
+    
+}
