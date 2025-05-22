4d2ec593f8c845506142125739fb7b4dce0e0f08
Azeel Sajjad <aasajjad05@gmail.com>
2025-05-22 15:15:31 -0400

    m

--- backend/controllers/rating.ts
+++ backend/controllers/rating.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express';
-import Preference, { IPREFERENCE, ratingContext } from '../models/Preference';
-import Ratings, { IRATINGS } from '../models/Rating';
+import Preference from '../models/Preference';
 import Drinks, { IDRINKS } from '../models/Drinks';
-import Users, { IUSER } from '../models/Users';
-import mongoose, { mongo } from 'mongoose';
-import { environment } from '../config/environment';
+import Users from '../models/Users';
+import mongoose from 'mongoose';
 
 export const initializeRating = async (req: Request, res: Response) => {
     try {
