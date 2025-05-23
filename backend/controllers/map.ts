3c074042b156d8640cac0777935191110e06382c
Azeel Sajjad <aasajjad05@gmail.com>
2025-05-22 20:08:04 -0400

    m

--- backend/controllers/map.ts
+++ backend/controllers/map.ts
@@ -12,6 +12,7 @@ export const searchCafeByName = async (req: Request, res: Response) => {
         const {name, loc} = req.query
         if(!name || !loc){
             res.status(400).json({message: 'Missing Fields'})
+            return
         }
         const api_key = process.env.GOOGLE_MAP_API_KEY
         const radius = 3000
@@ -24,10 +25,22 @@ export const searchCafeByName = async (req: Request, res: Response) => {
             const location = response.data.results[0].geometry.location as { lat: number; lng: number }
             const longitude = location.lng
             const latitude = location.lat
-            const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${encodedName}&location=${location}&radius=${radius}&type=${type}&key=${api_Key}`
+            const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${encodedName}&location=${latitude},${longitude}&radius=${radius}&type=${type}&key=${api_key}`
+            const newRes = await axios.get(placesUrl)
+            if(newRes.data.results.length > 0){
+                const places = newRes.data.results
+                const placeName = places[0].name
+                const address = places[0].vicinity
+                const placeId = response.data.results[0].place_id
+                const place_lng = places[0].geometry.location.lng
+                const place_lat = places[0].geometry.location.lat
+            } else {
+                res.status(404).json({message: 'Location not found'})
+            }
+        } else {
+            res.status(404).json({message: 'Location not found'})
+            return
         }
-        
-
     } catch (error) {
         
     }
